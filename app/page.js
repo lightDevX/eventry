0184a6b43f3088485bcb545013a06bbe449fde40
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-06-13 16:24:20 +0600

    header component create

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+import Header from "@/components/landingPage/header";
 
 export default function Home() {
   return (
-    <></>
+    <main className=" container mt-4">
+      <Header />
+    </main>
   );
 }
---
+++ components/landingPage/Search.jsx
@@ -0,0 +1,14 @@
+
+const Search = () => {
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Search an Event"
+                className="bg-[#27292F] border border-[#CCCCCC]/20 py-1 px-2 rounded-md"
+            />
+        </div>
+    );
+};
+
+export default Search;
