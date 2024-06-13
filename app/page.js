1d161c0804429cd2f32542f50d7d64077be31f9c
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-06-13 23:46:36 +0600

    EventCard component create

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import EventList from "@/components/landingPage/EventList";
 import Header from "@/components/landingPage/header";
 
 export default function Home() {
   return (
     <main className=" container mt-4">
       <Header />
+      <EventList />
     </main>
   );
 }
