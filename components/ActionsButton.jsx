f1cb0611e33d398b818f2c513a8b7a9272d90013
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-06-15 01:25:46 +0600

    payment method implement

--- components/ActionsButton.jsx
+++ components/ActionsButton.jsx
@@ -4,7 +4,7 @@ const ActionsButton = ({ fromDetails }) => {
     return (
         <div className={`w-full flex gap-4 mt-4 ${fromDetails && "flex-1"}`}>
             <button className="w-full bg-indigo-600 hover:bg-indigo-800">Interested</button>
-            <Link href="#" className="text-center w-full bg-[#464849] py-2 px-2 rounded-md border border-[#5F5F5F]/50 shadow-sm cursor-pointer hover:bg-[#3C3D3D] transition-colors active:translate-y-1">Going</Link>
+            <Link href="/payment" className="text-center w-full bg-[#464849] py-2 px-2 rounded-md border border-[#5F5F5F]/50 shadow-sm cursor-pointer hover:bg-[#3C3D3D] transition-colors active:translate-y-1">Going</Link>
         </div>
     );
 };
