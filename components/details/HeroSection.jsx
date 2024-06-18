e5ae800eeedf725c5ba49a1c79fb4e9476b8c252
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-06-18 11:55:05 +0600

    u

--- components/details/HeroSection.jsx
+++ components/details/HeroSection.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
                     <p className="text-[#9C9C9C] text-base mt-1">Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh</p>
                     <div className="text-[#737373] text-sm mt-1">
                         <span>1k Interested</span>
-                        <span>|</span>
+                        <span> | </span>
                         <span>40K Going</span>
                     </div>
                 </div>
