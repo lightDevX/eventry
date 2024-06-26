cc7c8bb053438a3b596ae14581facdcbc4d9efee
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-06-26 19:20:51 +0600

    u

--- app/payment/page.js
+++ app/payment/page.js
@@ -4,7 +4,7 @@ const PaymentPage = () => {
     return (
         <section className="container">
             <div className="bg-[#3d556d] p-6 rounded-lg max-w-xl mx-auto my-12">
-                <h2 className="font-bold text-xl mb-8">Payment Details</h2>
+                <h2 className="font-bold text-xl mb-8 text-pink-500">Payment Details</h2>
                 <PaymentForm />
             </div>
         </section>
