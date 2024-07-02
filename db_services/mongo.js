3eafbed6a467ece85ee57dc16688ca6a6219d874
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-07-02 10:18:48 +0600

    Mongoose install and Mongo URI connected

---
+++ db_services/mongo.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export async function dbConnect() {
+    try {
+        const conLink = await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+        return conLink;
+    } catch (e) {
+        console.log(e);
+    }
+}
