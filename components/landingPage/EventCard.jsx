16d19a76a5557e2a350b9699592b2b3db5f4c2ad
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-07-03 18:37:35 +0600

    connected to db and implement to Event list

--- components/landingPage/EventCard.jsx
+++ components/landingPage/EventCard.jsx
@@ -2,24 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 import ActionsButton from "../ActionsButton";
 
-const EventCard = () => {
+const EventCard = ({ allEvent }) => {
     return (
-        <div>
-            <div className="overflow-hidden rounded-md bg-[#242526]">
-                <Image src="/google-io-2023-1.png" alt="Event 1" className="w-full" width={500} height={500} />
-
-                <div className="p-3">
-                    <Link href="/details/1" className="font-bold text-lg">Google I/O Extended</Link>
-                    <p className="text-[#9C9C9C] text-sm mt-1">Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh</p>
-                    <div className="text-[#737373] text-sm mt-1">
-                        <span>1k Interested</span>
-                        <span> | </span>
-                        <span>40K Going</span>
+        <>
+            {
+                allEvent.map((event) => <div key={event._id}>
+                    <div className="overflow-hidden rounded-md bg-[#242526]">
+                        <Image src="/google-io-2023-1.png" alt="Event 1" className="w-full" width={500} height={500} />
+                        <div className="p-3">
+                            <Link href="/details/1" className="font-bold text-lg">{event.name}</Link>
+                            <p className="text-[#9C9C9C] text-sm mt-1">{event.location}</p>
+                            <div className="text-[#737373] text-sm mt-1">
+                                <span>1k Interested</span>
+                                <span> | </span>
+                                <span>40K Going</span>
+                            </div>
+                            <ActionsButton />
+                        </div>
                     </div>
-                    <ActionsButton />
-                </div>
-            </div>
-        </div >
+                </div>)
+            }
+        </ >
     );
 };
 
