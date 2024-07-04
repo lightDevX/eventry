daa38610a36ee065832c9b6588898db8355aadb6
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-07-04 12:31:01 +0600

    data utils create

--- components/landingPage/EventList.jsx
+++ components/landingPage/EventList.jsx
@@ -5,6 +5,7 @@ const EventList = async () => {
 
     const allEvent = await getAllEvents();
 
+    console.log(allEvent);
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
--- db/query.js
+++ db/query.js
@@ -1,8 +1,9 @@
 import { eventModel } from "@/models/event-model";
+import { replaceMongoIdInArray } from "@/utils/data-utils";
 
 async function getAllEvents() {
-    const allEvents = await eventModel.find();
-    return allEvents;
+    const allEvents = await eventModel.find().lean();
+    return replaceMongoIdInArray(allEvents);
 }
 
 export {
