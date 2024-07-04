96050306316149cb9aa66570f7fd9487312a1e6e
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-07-05 01:02:44 +0600

    make dynamic event list and cards

--- components/landingPage/EventCard.jsx
+++ components/landingPage/EventCard.jsx
@@ -2,26 +2,28 @@ import Image from "next/image";
 import Link from "next/link";
 import ActionsButton from "../ActionsButton";
 
-const EventCard = ({ allEvent }) => {
+const EventCard = ({ event }) => {
     return (
         <>
-            {
-                allEvent.map((event) => <div key={event._id}>
-                    <div className="overflow-hidden rounded-md bg-[#242526]">
-                        <Image src="/google-io-2023-1.png" alt="Event 1" className="w-full" width={500} height={500} />
-                        <div className="p-3">
-                            <Link href="/details/1" className="font-bold text-lg">{event.name}</Link>
-                            <p className="text-[#9C9C9C] text-sm mt-1">{event.location}</p>
-                            <div className="text-[#737373] text-sm mt-1">
-                                <span>1k Interested</span>
-                                <span> | </span>
-                                <span>40K Going</span>
-                            </div>
-                            <ActionsButton />
-                        </div>
+            <div className="overflow-hidden rounded-md bg-[#242526]">
+                <Image
+                    src={event?.imageUrl}
+                    alt="Event 1"
+                    width={500}
+                    height={500}
+                    className="w-full"
+                />
+                <div className="p-3">
+                    <Link href={`/details/${event?.id}`} className="font-bold text-lg">{event.name}</Link>
+                    <p className="text-[#9C9C9C] text-sm mt-1">{event.location}</p>
+                    <div className="text-[#737373] text-sm mt-1">
+                        <span>{event?.interested_ids?.length} Interested</span>
+                        <span className="mr-1"> | </span>
+                        <span>{event?.going_ids?.length} Going</span>
                     </div>
-                </div>)
-            }
+                    <ActionsButton />
+                </div>
+            </div>
         </ >
     );
 };
--- components/landingPage/EventList.jsx
+++ components/landingPage/EventList.jsx
@@ -9,7 +9,11 @@ const EventList = async () => {
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-            <EventCard allEvent={allEvent} />
+            {
+                allEvent.map((event) =>
+                    <EventCard key={event?.id} event={event} />
+                )
+            }
         </div>
     );
 };
