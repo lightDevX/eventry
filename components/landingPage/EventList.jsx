f57da1423496070c86560a698fc29a4d735262a3
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-07-06 18:00:13 +0600

    u

--- components/landingPage/EventList.jsx
+++ components/landingPage/EventList.jsx
@@ -8,7 +8,7 @@ const EventList = async () => {
     console.log(allEvent);
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 text-teal-200">
             {
                 allEvent.map((event) =>
                     <EventCard key={event?.id} event={event} />
